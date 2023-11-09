9705f95a93d876e2d1180802a15151631cc9121a
[Ayman Shiham] <sharktank38000@gmail.com>
2023-11-08 20:46:13 -0500

    implemented getDb and putDb method for adding , updating or retrieving all content from jate Obj store

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -21,9 +21,47 @@ export const putDb = async (content) => {
 
   // New transcation and defining the db and db entitlement
   const transAct = jateDb.transaction("jate", "readwrite");
+
+  // Accessing the Obj store within the transaction
+  const store = transAct.objectStore("jate");
+
+  // validating if the data with id 1 already exists
+  const existingData = await store.get(1);
+
+  if(existingData){
+    // if exist then Update it using put
+    return await store.put({id: 1, value: content});
+  }
+
+  // doesnt exist then add it using add
+  const addResult = await store.add({id: 1, value: content})
+
+  // complete transaction
+  await transAct.done;
+
+  return addResult;
+
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("get from DB")
 
+    // Connection to db and version designated to use
+    const jateDb =await openDB("jate", 1);
+
+    // New transaction and defining the db and db entitlement
+    const transAct = jateDb.transaction("jate", "readonly");
+
+    // Accessing the Obj store within the transaction
+    const store = transAct.objectStore("jate");
+
+    // Get all content from the Obj store
+    const allContent = await store.getAll();
+
+    // complete transaction
+    await transAct.done;
+
+    return allContent;
+}
 initdb();
