c39a484d0b4525084d0ba88ee98c83273dde7e8f
[Ayman Shiham] <sharktank38000@gmail.com>
2023-11-08 21:35:40 -0500

    Implemented PWA installation logic: show 'Install' button on 'beforeinstallprompt', handle click event to prompt installation, and reset variables on 'appinstalled' event.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,35 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+
+    // store the event
+    window.deferredPrompt = event;
+
+    // show btn they can install
+    butInstall.style.display = "block";
+});
+
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    // check if install prompt is available
+    if(!window.deferredPrompt){
+        return;
+    }
+
+    // showing prompt
+    window.deferredPrompt.prompt();
+
+    // reset the deferredprompt variable
+    window.deferredPrompt = null;
+
+    // hide the install Btn
+    butInstall.style.display = "none"
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // reset variable after installation
+    window.deferredPrompt = null
+});
